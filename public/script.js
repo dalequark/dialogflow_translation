1b35223e496ac0095c2b5983fb6a209f8fe74cc2
Dale Markowitz <dalequark@google.com>
2019-12-06 12:11:41 -0600

    please let me exit this rebase

--- public/script.js
+++ public/script.js
@@ -1,55 +1,55 @@
+RESPONSE_URL =
+  "http://localhost:5000/translationchat-hvjswp/us-central1/translateIntent";
 
-RESPONSE_URL = "http://localhost:5000/translationchat-hvjswp/us-central1/translateIntent";
+sessionId = String(Math.floor(Math.random() * 1000000000000));
 
-sessionId = String(Math.floor(Math.random() * 1000000000000))
+console.log("Hello world");
 
 function addNewMessage(msg, sender = "self") {
-    const divNode = document.createElement('div');
-    divNode.className = (sender == "self") ? "message us" : "message : them";
-    const textNode = document.createTextNode(msg);
-    divNode.appendChild(textNode);
-    document.getElementById('messages').appendChild(divNode);
+  const divNode = document.createElement("div");
+  divNode.className = sender == "self" ? "message us" : "message : them";
+  const textNode = document.createTextNode(msg);
+  divNode.appendChild(textNode);
+  document.getElementById("messages").appendChild(divNode);
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-async function postData(url = '', data = {}) {
-    // Default options are marked with *
-    const response = await fetch(url, {
-      method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-    //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    //   credentials: 'same-origin', // include, *same-origin, omit
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    //   redirect: 'follow', // manual, *follow, error
-    //   referrer: 'no-referrer', // no-referrer, *client
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
-    });
-    return await response.json(); // parses JSON response into native JavaScript objects
-  }
+async function postData(url, data = {}) {
+  // Default options are marked with *
 
-async function getResponse(msg) {
-    const jsonRes = await postData(RESPONSE_URL, {
-        "text" : msg,
-        "sessionId" : sessionId
-    } );
-    return jsonRes;
+  const response = await fetch(url, {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    mode: "cors", // no-cors, *cors, same-origin
+    // credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+
+  return response.json(); // parses JSON response into native JavaScript objects
 }
 
-document.getElementById('msg-form').addEventListener('submit', async function(e){
-    e.preventDefault()
-    const msg = document.getElementById('msg-form').elements[0].value;
+async function getResponse(msg) {
+  const jsonRes = await postData(RESPONSE_URL, {
+    text: msg,
+    sessionId: sessionId
+  });
+  console.log(jsonRes);
+  return jsonRes;
+}
+
+document
+  .getElementById("msg-form")
+  .addEventListener("submit", async function(e) {
+    e.preventDefault();
+    const msg = document.getElementById("msg-form").elements[0].value;
     addNewMessage(msg);
-    document.getElementById('msg-form').reset();
-    document.getElementById('msg-input').innerText = '';
+    document.getElementById("msg-form").reset();
+    document.getElementById("msg-input").innerText = "";
     console.log(msg);
-    getResponse(msg).then((reply) => {
-        addNewMessage(reply.replyText, "them");
+    getResponse(msg).then(reply => {
+      addNewMessage(reply.replyText, "them");
     });
     return true;
-});
-
-
+  });
